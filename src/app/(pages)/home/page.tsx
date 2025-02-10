8a0a0c0a83b00dc3e376af9dacb7085f8e27e400
Thayano Davalo <ttdavalo@gmail.com>
2025-02-10 03:39:11 -0300

    feat: add features in product pages

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -3,7 +3,6 @@ import { HomeComponent } from './_components/home.component'
 
 export default function Home() {
   let instagramFeed = null;
-  let error = null;
   async function getPost() {
     try {
       const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,media_type,timestamp,permalink&access_token=IGAAJSIIrCP1lBZAE1mdFFETFMtY1FoMTB0YVBwSjlURVFXVDk1dGM0VENpNDJIM2xUMENaSzFaS25pMlZASZAzZAkTVc4T19ST0ZAwa0pqSGZAta2NYNGl1U19iNkF0ei1VeHFVTFFtQ2YwNlk5M3I2dHZAyaGgwQzlNNjZAfenZAoVU82OAZDZD`;
@@ -14,9 +13,8 @@ export default function Home() {
       }
       instagramFeed = await data.json();
       console.log("Instagram feed:", instagramFeed);
-    } catch (err: any) {
-      console.error("Error fetching Instagram feed:", err.message);
-      error = err.message;
+    } catch {
+      console.error("Error fetching Instagram feed:");
 
     }
   }
